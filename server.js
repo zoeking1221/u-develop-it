03bc56fa86879ad897f0496f8ebd626249887545
Zoe King <zoeking@Zoes-MacBook-Pro.local>
2021-01-24 22:35:21 -0500

    joined with party table

--- server.js
+++ server.js
@@ -19,8 +19,12 @@ const db = new sqlite3.Database('./db/election.db', err => {
 
 // GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates
-                    WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id
+                WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if (err) {
@@ -37,7 +41,11 @@ app.get('/api/candidate/:id', (req, res) => {
 
 // GET all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
         if (err) {
